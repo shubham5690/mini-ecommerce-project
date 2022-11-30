b8278a552b5886f1932a492b50cdd04ef51c4604
SHUBHAM CHOUDHARY <101628237+shubham5690@users.noreply.github.com>
2022-11-30 12:46:28 -0500

    Add files via upload

---
+++ mini_ecommerce_project/src/test/java/com/masai/mini_ecommerce_project/MiniEcommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masai.mini_ecommerce_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniEcommerceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
